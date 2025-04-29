b5009c705f1f21db02ebc6d077d0ceeafc3caa32
alu0100536690 <merquis85@gmail.com>
2025-04-30 00:20:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,5 @@
 import streamlit as st
-from scraping_module import render_sidebar as render_scraping_sidebar, render as render_scraping
-from cpt_triptoislands import render_sidebar as render_cpt_sidebar, render as render_cpt
+from scraping_module import render_scraping
 
-st.set_page_config(page_title="TripToIslands Panel", layout="wide")
-
-# ─── MENÚ ───────────────────────────────────────────────────────────────────────
-menu = st.radio("Selecciona módulo", ["Relaciones CPT", "Scraping Google"], horizontal=True)
-
-# ─── MÓDULOS ────────────────────────────────────────────────────────────────────
-if menu == "Relaciones CPT":
-    site_url, post_type, per_page = render_cpt_sidebar()
-    render_cpt(site_url, post_type, per_page)
-
-elif menu == "Scraping Google":
-    etiquetas = render_scraping_sidebar()
-    render_scraping(etiquetas)
+st.set_page_config(page_title="TripToIslands Scraper", layout="wide")
+render_scraping()
