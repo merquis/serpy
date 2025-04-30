fd54110ae8e22938b614dcee2c1d969c69d844b6
alu0100536690 <merquis85@gmail.com>
2025-04-30 19:10:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 from scraping_module import render_scraping
+# from otro_modulo import render_otra_funcion
 
-st.set_page_config(page_title="TripToIslands Scraper", layout="wide")
-render_scraping()
+st.set_page_config(page_title="TripToIslands Admin", layout="wide")
+
+st.sidebar.title("TripToIslands · Panel Admin")
+opcion = st.sidebar.selectbox("Selecciona un módulo", ["Scraping"])
+
+if opcion == "Scraping":
+    render_scraping()
+# elif opcion == "Otro módulo":
+#     render_otra_funcion()
