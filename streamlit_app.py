217662531ce905acadb413adad72d2572d984edc
alu0100536690 <merquis85@gmail.com>
2025-05-02 21:31:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # streamlit_app.py
 import streamlit as st
 from scrapers.scraping_google_url import render_scraping_google_urls
-from scraping_etiquetas_url import render_scraping_etiquetas_url
+from scrapers.scraping_etiquetas_url import render_scraping_etiquetas_url
 from cpt_module import render_cpt_module
 
 
