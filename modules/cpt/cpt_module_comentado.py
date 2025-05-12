c9a7be6017e2c8ac7f9cfb8483ba9f4302ac9069
alu0100536690 <merquis85@gmail.com>
2025-05-12 22:13:48 +0200

    Delete modules directory

--- modules/cpt/cpt_module_comentado.py
+++
@@ -1,7 +0,0 @@
-# Importación de librerías necesarias
-import streamlit as st
-
-# Definición de función
-def render_cpt_module():
-    st.title("🔍 Módulo CPT")
-    st.write("👋 ¡Hola! Este es el módulo de gestión de CPTs.")
