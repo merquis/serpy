1cf8833554d4d900ca0e78f6d502f3d69798940f
alu0100536690 <merquis85@gmail.com>
2025-05-12 22:22:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Asegurarnos que 'modules' esté accesible
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 import streamlit as st
 
 # ══ MÓDULOS FUNCIONALES ══
