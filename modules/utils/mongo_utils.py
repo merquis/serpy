846c82320d57be4f8a024340cd78ea784ceacd9d
alu0100536690 <merquis85@gmail.com>
2025-05-17 22:36:42 +0200

    Update mongo_utils.py

--- modules/utils/mongo_utils.py
+++ modules/utils/mongo_utils.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 
 def subir_a_mongodb(data: dict, db_name: str, collection_name: str, uri: str = None):
-    uri = uri or "mongodb://serpy:Esperanza#85@serpy_mongodb:27017/?authSource=admin"
+    # URI corregida con la nueva contraseña (esperanza85) sin caracteres especiales
+    uri = uri or "mongodb://serpy:esperanza85@serpy_mongodb:27017/?authSource=admin"
     client = MongoClient(uri)
     db = client[db_name]
     collection = db[collection_name]
