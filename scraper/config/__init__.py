0611ef337ff15c51653eefb54eed7b34d8e9fef6
alu0100536690 <merquis85@gmail.com>
2025-06-11 20:18:04 +0200

    a

--- scraper/config/__init__.py
+++ scraper/config/__init__.py
@@ -1,3 +1,3 @@
-from .settings import config, AppConfig, ScrapingConfig, UIConfig
+from .settings import settings, normalize_project_name
 
-__all__ = ['config', 'AppConfig', 'ScrapingConfig', 'UIConfig'] 
+__all__ = ['settings', 'normalize_project_name']
