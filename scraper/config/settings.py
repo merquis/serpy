2917eb3bb1d1a431cda31cd43aa6abef7a5ac4e8
alu0100536690 <merquis85@gmail.com>
2025-06-14 19:08:33 +0200

    añadiendo api gemini

--- scraper/config/settings.py
+++ scraper/config/settings.py
@@ -127,10 +127,10 @@ class Settings(BaseSettings):
             },
             "Google Gemini": {
                 "models": [
-                    "gemini-2.5-flash-preview-05-20",
-                    "gemini-2.5-pro-preview-06-05"
+                    "gemini-2.5-flash-preview",
+                    "gemini-2.5-pro-preview"
                 ],
-                "default": "gemini-2.5-flash-preview-05-20"
+                "default": "gemini-2.5-flash-preview"
             }
         }
     )
